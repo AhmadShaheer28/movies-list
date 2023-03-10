3bc8e41bbc6e6ae6a4fe4b55e69bb6cc4048b7fb
Ahmad Shaheer <ahmadshaheer28@gmail.com>
2023-03-10 22:39:28 +0500

    project setup

---
+++ MovieListAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieListAssignment
+//
+//  Created by Ahmad Shaheer on 10/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
